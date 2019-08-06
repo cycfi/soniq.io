f2ee21d56168739763cadc4f4f6e25e2b0c36dfc
djowel <djowel@gmail.com>
2019-08-06 15:09:37 +0800

    yep. OLED works in F446

---
+++ include/inf/canvas.hpp
@@ -0,0 +1,231 @@
+/*=============================================================================
+   Copyright (c) 2015-2017 Cycfi Research. All rights reserved.
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#if !defined(CYCFI_INFINITY_CANVAS_HPP_MAY_9_2017)
+#define CYCFI_INFINITY_CANVAS_HPP_MAY_9_2017
+
+#include <utility>
+#include <cstdint>
+#include <string>
+
+namespace cycfi { namespace infinity
+{
+   ////////////////////////////////////////////////////////////////////////////
+   // Simple basic types for monochrome graphics
+   ////////////////////////////////////////////////////////////////////////////
+   namespace monochrome
+   {
+      enum class color
+      {
+         white,
+         black,
+         inverse,
+      };
+
+      enum class font
+      {
+         small,
+         medium,
+         large,
+      };
+
+      struct bitmap
+      {
+         std::uint8_t const* data;
+         int width;
+         int height;
+      };
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // mono_canvas canvas. This is implements routines for drawing
+   // into monochromatic OLED frame buffers.
+   ////////////////////////////////////////////////////////////////////////////
+   template <std::size_t width_, std::size_t height_>
+   struct mono_canvas
+   {
+   public:
+
+      enum { width  = width_, height = height_ };
+      using color = monochrome::color;
+      using font = monochrome::font;
+      using bitmap = monochrome::bitmap;
+      static std::size_t const size = (width * height) / 8;
+
+      mono_canvas(std::uint8_t* buffer_)
+       : _buffer(buffer_)
+      {
+         clear();
+      }
+
+      void  clear();
+
+      void  draw_pixel(int x, int y, color color_ = color::white);
+      void  draw_line(int x0, int y0, int x1, int y1, color color_ = color::white);
+      void  fill_rect(int x, int y, int w, int h, color color_ = color::white);
+      void  draw_rect(int x, int y, int w, int h, color color_ = color::white);
+      void  draw_bitmap(bitmap const& img, int x, int y, color color_ = color::white);
+
+      void  fast_hline(int x, int y, int w, color color_ = color::white);
+      void  fast_vline(int x, int y, int h, color color_ = color::white);
+
+      int   draw_char(char ch, int x, int y, font font_, color color_ = color::white);
+      int   draw_string(char const* str, int x, int y, font font_, color color_ = color::white);
+      int   draw_string(std::string const& str, int x, int y, font font_, color color_ = color::white);
+
+      std::uint8_t*           begin()        { return _buffer; }
+      std::uint8_t const*     begin() const  { return _buffer; }
+      std::uint8_t*           end()          { return _buffer + size; }
+      std::uint8_t const*     end() const    { return _buffer + size; }
+
+      std::uint8_t&           operator[](std::size_t i)        { return _buffer[i]; }
+      std::uint8_t const      operator[](std::size_t i) const  { return _buffer[i]; }
+
+   private:
+
+      std::uint8_t* _buffer;
+   };
+
+   ////////////////////////////////////////////////////////////////////////////
+   // canvas implementation
+   ////////////////////////////////////////////////////////////////////////////
+   namespace detail
+   {
+      void fast_hline_impl(
+         std::uint8_t* buffer, int width, int height,
+         int x, int y, int w, monochrome::color color_
+      );
+
+      void fast_vline_impl(
+         std::uint8_t* buffer, int width, int height,
+         int16_t x, int16_t y_, int16_t h_, monochrome::color color_
+      );
+
+      void line_impl(
+         std::uint8_t* buffer, int width, int height,
+         int x0, int y0, int x1, int y1, monochrome::color color_
+      );
+
+      void draw_bitmap(
+          std::uint8_t* buffer, std::uint8_t* end, int width, int height,
+          int x, int y, monochrome::bitmap const& bm, monochrome::color color_
+      );
+
+      inline void draw_byte(std::uint8_t& out, std::uint8_t pix, monochrome::color color_)
+      {
+         switch (color_)
+         {
+            case monochrome::color::white:   out |=  pix;  break;
+            case monochrome::color::black:   out &= ~pix;  break;
+            case monochrome::color::inverse: out ^=  pix;  break;
+         }
+      };
+
+      inline void draw_pixel(
+         std::uint8_t* buffer, int width, int height,
+         int16_t x, int16_t y, monochrome::color color_)
+      {
+         if ((x < 0) || (x >= width) || (y < 0) || (y >= height))
+            return;
+
+         auto mask = y&7;
+         auto* p = buffer + (x + (y / 8) * width);
+         draw_byte(*p, 1 << mask, color_);
+      }
+
+      int draw_char(
+          std::uint8_t* buffer, std::uint8_t* end, int width, int height,
+          int x, int y, char ch, monochrome::font font_, monochrome::color color_
+      );
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline void mono_canvas<width, height>::clear()
+   {
+      for (auto& pixels : *this)
+         pixels = 0;
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline void mono_canvas<width, height>::fast_hline(int x, int y, int w, color color_)
+   {
+      detail::fast_hline_impl(_buffer, width, height, x, y, w, color_);
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline void mono_canvas<width, height>::fast_vline(int x, int y, int h, color color_)
+   {
+      detail::fast_vline_impl(_buffer, width, height, x, y, h, color_);
+   }
+
+   template <std::size_t width, std::size_t height>
+   void mono_canvas<width, height>::draw_pixel(int x, int y, color color_)
+   {
+      detail::draw_pixel(_buffer, width, height, x, y, color_);
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline void mono_canvas<width, height>::draw_line(int x0, int y0, int x1, int y1, color color_)
+   {
+      if (x0 == x1)
+      {
+         if (y0 > y1)
+            std::swap(y0, y1);
+         fast_vline(x0, y0, y1 - y0 + 1, color_);
+      }
+      else if (y0 == y1)
+      {
+         if(x0 > x1)
+            std::swap(x0, x1);
+         fast_hline(x0, y0, x1 - x0 + 1, color_);
+      }
+      else
+      {
+         detail::line_impl(_buffer, width, height, x0, y0, x1, y1, color_);
+      }
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline void mono_canvas<width, height>::fill_rect(int x, int y, int w, int h, color color_)
+   {
+      for (auto i=x; i<x+w; ++i)
+         fast_vline(i, y, h, color_);
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline void mono_canvas<width, height>::draw_rect(int x, int y, int w, int h, color color_)
+   {
+      fast_hline(x, y, w, color_);
+      fast_hline(x, y+h-1, w, color_);
+      fast_vline(x, y, h, color_);
+      fast_vline(x+w-1, y, h, color_);
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline int mono_canvas<width, height>::draw_char(char ch, int x, int y, font font_, color color_)
+   {
+      return detail::draw_char(_buffer, _buffer+size, width, height, x, y, ch, font_, color_);
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline int mono_canvas<width, height>::draw_string(char const* str, int x, int y, font font_, color color_)
+   {
+      if (str)
+      {
+         while (*str)
+            x = draw_char(*str++, x, y, font_, color_);
+         return x;
+      }
+      return 0;
+   }
+
+   template <std::size_t width, std::size_t height>
+   inline int mono_canvas<width, height>::draw_string(std::string const& str, int x, int y, font font_, color color_)
+   {
+      return draw_string(str.c_str(), x, y, font_, color_);
+   }
+}}
+
+#endif
---
+++ include/inf/ssd1306.hpp
@@ -0,0 +1,238 @@
+/*=============================================================================
+   Copyright (c) 2015-2017 Cycfi Research. All rights reserved.
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#if !defined(CYCFI_INFINITY_SSD_1306_HPP_AUGUST_16_2017)
+#define CYCFI_INFINITY_SSD_1306_HPP_AUGUST_16_2017
+
+#include <inf/support.hpp>
+#include <inf/canvas.hpp>
+#include <cstdint>
+
+namespace cycfi { namespace infinity
+{
+   ////////////////////////////////////////////////////////////////////////////
+   // ssd1306 driver
+   ////////////////////////////////////////////////////////////////////////////
+   template <typename Port, typename Canvas, std::size_t timeout_ = 0xffffffff>
+   struct ssd1306 : Canvas
+   {
+   public:
+
+      using port = Port;
+      using canvas = Canvas;
+
+      static std::size_t const timeout = timeout_;
+      static std::size_t const height = Canvas::height;
+      static std::size_t const width = Canvas::width;
+      static std::size_t const buffer_size = Canvas::size + 1;
+
+      ssd1306(Port& out);
+
+      void refresh();
+      void on();
+      void off();
+
+      void bright(bool val = true);
+      void brightness(std::uint8_t val);
+
+   private:
+
+      void command(std::uint8_t cmd);
+      void out(std::uint8_t const* data, std::size_t len);
+
+      std::uint8_t   _buffer[buffer_size];
+      Port&          _out;
+   };
+
+   ////////////////////////////////////////////////////////////////////////////
+   // ssd1306 implementation
+   ////////////////////////////////////////////////////////////////////////////
+   namespace ssd1306_constants
+   {
+      enum
+      {
+         set_mux_ratio              = 0xa8,
+         charge_pump                = 0x8d,
+         enable_charge_val          = 0x14,
+         set_pre_charge             = 0xd9,
+         pre_charge_value           = 0x10,
+         vcom_detect_value          = 0x10,
+
+         memory_mode                = 0x20,
+
+         set_contrast               = 0x81,
+         default_contrast           = 0x8F,
+
+         set_display_clock_div      = 0xd5,
+         set_display_offset         = 0xd3,
+         set_start_line             = 0x40,
+
+         seg_remap                  = 0xa0,
+         com_scan_inc               = 0xc0,
+         com_scan_dec               = 0xc8,
+
+         set_com_pins               = 0xda,
+         set_vcom_detect            = 0xdb,
+         display_all_on_resume      = 0xa4,
+         display_all_on             = 0xa5,
+         normal_display             = 0xa6,
+         invert_display             = 0xa7,
+
+         display_off                = 0xae,
+         display_on                 = 0xaf,
+
+         activate_scroll            = 0x2f,
+         deactivate_scroll          = 0x2e,
+
+         set_low_column             = 0x00,
+         set_high_column            = 0x10,
+         column_addr                = 0x21,
+         page_addr                  = 0x22,
+
+   		ssd1306_i2c_addr           = 0x78
+      };
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   inline void ssd1306<Port, Canvas, timeout>::command(std::uint8_t cmd)
+   {
+      uint8_t buf[2] = { 0, cmd };
+      out(buf, 2);
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   inline void ssd1306<Port, Canvas, timeout>::out(std::uint8_t const* data, std::size_t len)
+   {
+      using namespace ssd1306_constants;
+      _out.write(ssd1306_i2c_addr, data, len, timeout);
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   inline ssd1306<Port, Canvas, timeout>::ssd1306(Port& out)
+    : Canvas(&_buffer[1])
+	 , _out(out)
+   {
+      using namespace ssd1306_constants;
+
+      // place the 0x40 prefix
+      _buffer[0] = 0x40;
+
+      // Turn display off
+      off();
+
+      // Wait for DC-DC to stabilise.
+      delay_ms(100);
+
+      // Set MUX ratio
+      command(set_mux_ratio);
+      command(height - 1);
+
+      // Enable charge pump
+      command(charge_pump);
+      command(enable_charge_val);
+
+      // Set the overall backlight brightness
+      bright();
+
+      // Memory mode: act like ks0108
+      command(memory_mode);
+      command(0x00);
+
+      // Set default contrast
+      command(set_contrast);
+      command(default_contrast);
+
+      command(set_display_clock_div);
+      command(0x80);
+
+      // Set no display offset
+      command(set_display_offset);
+      command(0x0);
+      command(set_start_line | 0x0);
+
+      command(seg_remap | 0x1);
+      command(com_scan_dec);
+
+      command(set_com_pins);
+      if (width == 128 && height == 32)
+         command(0x02);
+      else if (width == 64 && height == 32)
+         command(0x12);
+      else if (width == 96 && height == 16)
+         command(0x2);
+
+      command(display_all_on_resume);
+      command(normal_display);
+      command(deactivate_scroll);
+
+      // Clear the canvas
+      Canvas::clear();
+      refresh();
+
+      // Turn display on
+      on();
+
+      // Wait 150ms for display to turn on
+      delay_ms(150);
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   inline void ssd1306<Port, Canvas, timeout>::refresh()
+   {
+      using namespace ssd1306_constants;
+
+      command(column_addr);
+      command(0);                   // Column start address (0 = reset)
+      command(width-1);             // Column end address (127 = reset)
+
+      command(page_addr);
+      command(0);                   // Page start address (0 = reset)
+
+      if (height == 64)
+          command(7);               // Page end address
+      else if (height == 32)
+          command(3);               // Page end address
+      else if (height == 16)
+          command(1);               // Page end address
+
+      out(_buffer, buffer_size);
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   inline void ssd1306<Port, Canvas, timeout>::on()
+   {
+      using namespace ssd1306_constants;
+      command(display_on);
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   inline void ssd1306<Port, Canvas, timeout>::off()
+   {
+      using namespace ssd1306_constants;
+      command(display_off);
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   void ssd1306<Port, Canvas, timeout>::brightness(std::uint8_t val)
+   {
+      using namespace ssd1306_constants;
+      command(set_contrast);
+      command(val);
+      command(set_vcom_detect);
+      command(val / 4);
+   }
+
+   template <typename Port, typename Canvas, std::size_t timeout>
+   void ssd1306<Port, Canvas, timeout>::bright(bool val)
+   {
+      using namespace ssd1306_constants;
+      command(set_pre_charge);
+      command(val ? 0xf1 : pre_charge_value);
+      command(set_vcom_detect);
+      command(val ? 0x40 : vcom_detect_value);
+   }
+}}
+
+#endif
